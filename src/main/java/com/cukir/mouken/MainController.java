b6618ee852546949beee5feb0c285df11af2c83e
ryuu <ryuu.public@gmail.com>
2023-02-17 13:30:47 +0900

    fix: test

--- src/main/java/com/cukir/mouken/MainController.java
+++ src/main/java/com/cukir/mouken/MainController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping("test")
+    @GetMapping("/")
     public String test() {
-        return "test";
+        return "index";
     }
 }

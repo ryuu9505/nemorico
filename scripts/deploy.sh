9e3cedc4cce49b45888c0ae894be842bb2564ea0
ryuu <ryuu.public@gmail.com>
2023-02-17 11:53:16 +0900

    feat: for automatic deplyment

---
+++ scripts/deploy.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/app/step2
+PROJECT_NAME=mouken
+
+echo "> Copy Build files"
+
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo "> check PID of application now running"
+
+CURRENT_PID=$(pgrep -fl mouken | grep jar | awk '{print $1}')
+# CURRENT_PID=$(pgrep -f $PROJECT_NAME)
+
+echo "> pid : $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+        echo "there is no apps running:"
+else
+        echo "> kill -15 $CURRENT_PID"
+        kill -15 $CURRENT_PID
+        sleep 5
+fi
+
+echo "> deploy new app"
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+echo "> authorization $JAR_NAME"
+
+chmod +x $JAR_NAME
+
+echo "> Run $JAR_NAME"
+
+nohup java -jar \
+	-Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-db.yml \
+	$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
--- src/main/java/com/cukir/mouken/MoukenApplication.java
+++ src/main/java/com/cukir/mouken/MoukenApplication.java
@@ -2,12 +2,15 @@ package com.cukir.mouken;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class MoukenApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MoukenApplication.class, args);
+        SpringApplication application = new SpringApplication(MoukenApplication.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
     }
 
 }

89cc16beb8bd2eef5e12c4ed4dcf41ce1ca4ba90
ryuu <ryuu.public@gmail.com>
2023-02-17 12:27:39 +0900

    feat: allow static resources

--- src/main/java/com/cukir/mouken/SecurityConfig.java
+++ src/main/java/com/cukir/mouken/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.cukir.mouken;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,5 +20,12 @@ public class SecurityConfig {
                         .anyRequest().authenticated());
 
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .mvcMatchers("/node_modules/**", "/assets/**", "/css/**")
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }
